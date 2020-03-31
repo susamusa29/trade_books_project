db3f11d19f73a27fc4e29af1189d4df7d7aeac5b
Yee Hou Teoh <2471020t@student.gla.ac.uk>
2020-03-31 21:24:38 +0800

    search page, edited url and formatted url

--- tradebooks/urls.py
+++ tradebooks/urls.py
@@ -1,13 +1,18 @@
 from django.urls import path
 from tradebooks import views
+"""
+note:
+# added the index view here
+# all good - From Stanislva
+changed '' to index instead of login as it makes more sense
 
-# added the index view here 
-# all good - From Stanislva 
-
+author: Teoh Yee Hou (2471020t)
+        Stanislava Dyakova (2390717d)
+"""
 app_name = 'tradebooks'
 
 urlpatterns = [
-    path('', views.login, name='login'),
+    path('', views.index, name='index'),
     path('index/', views.index, name='index'),
     path('login/', views.user_login, name='login'),
     # maybe we can use slug for individual products
--- tradebooks/views.py
+++ tradebooks/views.py
@@ -1,22 +1,25 @@
 """django page views.
 
+note:
+formatted code, commented unused imports
+
 author: Teoh Yee Hou (2471020t)
         Stanislava Dyakova (2390717d)
 """
 
 # non-django
-from datetime import datetime
-    #new one added because of registration form
+# from datetime import datetime
+# new one added because of registration form
 from tradebooks.forms import UserForm, UserProfileForm
 
 # django
 
-from django.shortcuts import redirect, render
-from django.urls import reverse
-from django.contrib.auth import authenticate, login, logout
-from django.contrib.auth.decorators import login_required
-from django.http import HttpResponse
-
+from django.shortcuts import render
+# from django.shortcuts import redirect, render
+# from django.urls import reverse
+# from django.contrib.auth import authenticate, login, logout
+# from django.contrib.auth.decorators import login_required
+# from django.http import HttpResponse
 
 
 def index(request):
@@ -33,8 +36,10 @@ def user_login(request):
     """User login view."""
     return render(request, 'tradebooks/login.html')
 
+
 def register(request):
-    #a value to tell the TEMPLATE whether registration was successful
+    """Register view."""
+    # a value to tell the TEMPLATE whether registration was successful
     registered = False
 
     # if HTTP POST, then process form data
@@ -44,36 +49,38 @@ def register(request):
         profile_form = UserProfileForm(request.POST)
 
         if(user_form.is_valid() and profile_form.is_valid()):
-            #save data to database
+            # save data to database
             user = user_form.save()
 
-            #hash the data
+            # hash the data
             user.set_password(user.password)
             user.save()
 
-            #delay the saving the model
+            # delay the saving the model
             profile = profile_form.save(commit=False)
             profile.user = user
 
-            #handle a picture if the user provided it
+            # handle a picture if the user provided it
             if 'picture' in request.FILES:
                 profile.picture = request.FILES['picture']
-            
-            #save UserProfile model instance
+
+            # save UserProfile model instance
             profile.save()
 
-            registered =True
+            registered = True
         else:
-            #in case invalid
+            # in case invalid
             print(user_form.errors, profile_form.errors)
     else:
         # not HTTP POST
         user_form = UserForm()
         profile_form = UserProfileForm()
-    
-    return render(request, 'tradebooks/register.html', context = {'user_form': user_form, 'profile_form': profile_form,'registered': registered})
-        
 
+    return render(request,
+                  'tradebooks/register.html',
+                  context={'user_form': user_form,
+                           'profile_form': profile_form,
+                           'registered': registered})
 
 
 def product(request):
